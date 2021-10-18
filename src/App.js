158e1fbc8e3ed291b5e6bb832b3b6d34d631587c
dhiami <dhiamissaoui12121@gmail.com>
2021-10-18 13:07:45 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import "./App.css";
+import AddTodo from "./Components/Addtodo"
+import Todolist from "./Components/todolist";
+import { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([
+    { text: "save the galaxy", id: 1, isDone: false },
+    { text: "walk the cat ", id: 2, isDone: false },
+    { text: "attend the workshop ", id: 3, isDone: false },
+  ]);
+
+  const deleteHandler = (ID) => {
+    setTodos(todos.filter((el) => el.id !== ID));
+  };
+
+  const doneHandler = (ID) => {
+    setTodos(
+      todos.map((el) => (el.id === ID ? { ...el, isDone: !el.isDone } : el))
+    );
+    console.log(todos.map((el) => el.id));
+  };
+  const [input, setInput] = useState("");
+  const addHandler = (newTodo) => {
+    input&&
+    setTodos([...todos, newTodo])
+  };
+  return (
+    <div className="App">
+      <h1>TODO APP!!</h1>
+      <AddTodo addHandler={addHandler} input={input} setInput={setInput} />
+      <Todolist
+        todoList={todos}
+        deleteHandler={deleteHandler}
+        doneHandler={doneHandler}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/todo.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Todo({ task, deleteHandler, doneHandler }) {
+  const deleteTodo = () => {
+    deleteHandler(task.id);
+  };
+
+  const doneTodo = () => {
+    doneHandler(task.id);
+  };
+  return (
+    <div className="todoContainer">
+      <h3
+        className={task.isDone ? "done" : "undone"}
+        style={task.isDone ? { backgroundColor: "blue" } : {}}
+      >
+        {" "}
+        {task.text}{" "}
+      </h3>
+      <div>
+        <button onClick={deleteTodo}> delete </button>
+        <button onClick={doneTodo}> {task.isDone ? "unDone" : "Done"} </button>
+      </div>
+    </div>
+  );
+}
